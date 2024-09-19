db6a317df6a6b7b97e65080339a5ca4778650591
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-09-19 17:19:09 +0300

    ready hw-11

--- src/main.js
+++ src/main.js
@@ -4,50 +4,66 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { API_KEY, BASE_URL } from "./js/pixabay-api";
-// import { showBox } from "./js/render-functions";
-// import { markup } from "./js/render-functions";
+import { createGalleryMarkup } from "./js/render-functions";
 
-const searchInput = document.querySelector('.search-input');
-const btnSubmit = document.querySelector('.btn');
+const form = document.querySelector('form.js-search-form');
 const gallery = document.querySelector('.gallery');
 
-// Приймаємо значення з інпуту
-function inputSearchValue () {
-   let searchValue = searchInput.value;
-    return searchValue;
-    }
+form.addEventListener('submit', onButtonSubmit);
 
+const showBox = new SimpleLightbox('.img-box a', { 
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.7,
+    className: 'lightbox',
+   });
 
-// Колбек при натисканні кнопки + запит на сервер
+// Функції для запуску та закриття лоадера
+function showLoader() {
+document.getElementById('loader').style.display = 'block';
+}
+
+function hideLoader() {
+    document.getElementById('loader').style.display = 'none';
+}
+
+   // Колбек при натисканні кнопки + запит на сервер
 function onButtonSubmit (event) {
     event.preventDefault();
+    showLoader();
 
-    // Оновлюю значення інпуту на момент натискання кнопки
-let searchValue = inputSearchValue(); 
-console.log (searchValue);
+const forma = event.currentTarget;
+const {
+    searchValue: { value: query },
+  } = forma.elements;
+console.log(query);
 
-   // Перевірка на пустий інпут    
-if (searchValue === "") {
+// let searchValue = inputSearchValue();
+ 
+// Перевірка на пустий інпут    
+if (query === "") {
     iziToast.show({
         message: `⚠️ Please fill search input`,
-        position: 'topCenter',
+        position: 'topRight',
         color: 'yellow',
     })
+     hideLoader();
 return
 }
 
-// Створюю обʼєкт з опціями запиту
-let options = {
+const options = {
     key: API_KEY,
-    q: searchValue,
+    q: query,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: "true",
 };
 
+
 // Перетворюю обʼєкт в урл формат запиту
-const params = new URLSearchParams(options).toString();
-  console.log(params); // Для проверки правильности параметров
+const params = new URLSearchParams(options);
+  console.log(params.toString()); // Для проверки правильности параметров
 
 //   зачистка попередньої видачі
 gallery.innerHTML = "";  
@@ -63,81 +79,19 @@ fetch(`${BASE_URL}api/?${params}`)
   .then((photos) => {
     if(!photos.hits || photos.hits.length === 0) {
         iziToast.show({
+            title: "❌",
             message: `"Sorry, there are no images matching your search query. Please try again!"`,
-            position: 'topCenter',
-            color: 'yellow',
+            position: 'topRight',
+            color: 'red',
         })
         return
     }
-else {
     console.log(photos);
-
-    const markup = photos.hits.map((photo) => {
-        return `<li class="li-gallery">
-      <div class="img-box">
-    <a class="gallery-link" href="${photo.largeImageURL}">
-        <img
-          class="gallery-image"
-          src="${photo.webformatURL}"
-          alt="${photo.tags}"
-          title="${photo.tags}"/>
-      </a>
-      </div>
-      <div class="description-box">
-        <div class="param-boxes"><p class="parameters">likes</p>
-        <p class="values">${photo.likes}</p></div>
-        <div class="param-boxes"><p class="parameters">views</p>
-        <p class="values">${photo.views}</p></div>
-        <div class="param-boxes"><p class="parameters">comments</p>
-        <p class="values">${photo.comments}</p></div>
-        <div class="param-boxes"><p class="parameters">downloads</p>
-        <p class="values">${photo.downloads}</p></div>
-      </div>
-      </li>`}).join("");
-
-    gallery.insertAdjacentHTML("beforeend", markup);
-    // Дані від бекенда
-          
-      }})
-  .catch((error) => console.log(error));  
+    gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(photos.hits));
+    showBox.refresh();
+})
+      
+  .catch((error) => console.log(error))
+  .finally(() => {hideLoader()}); 
 }
 
-
-
-export let showBox = new SimpleLightbox('.img-box a', { 
-    captions: true,
-    captionsData: 'alt',
-    captionDelay: 250,
-    overlayOpacity: 0.7,
-    className: 'lightbox',
-   });
-showBox.on('show.simplelightbox', function () {
-	// Do something…
-});
-
-showBox.on('error.simplelightbox', function (e) {
-	console.log(e); // Some usefull information
-});
-
-// with jQuery nearly the same
-//  showBox = $('.img-box a').simpleLightbox();
-// showBox.on('show.simplelightbox', function () {
-// 	// Do something…
-// });
-
-
-
-
-// new SimpleLightbox('.img-box a', { 
-//     captions: true,
-//     captionsData: 'alt',
-//     captionDelay: 250,
-//     overlayOpacity: 0.7,
-//     className: 'lightbox',
-//    });
-
-
-
-
-btnSubmit.addEventListener('click', onButtonSubmit);
-searchInput.addEventListener('input', inputSearchValue);
