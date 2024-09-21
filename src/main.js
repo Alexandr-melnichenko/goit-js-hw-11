8338f000e34379a39f143116294f7b3baa3778d1
Alexandr Melnichenko <alexandr.melnichenko@gmail.com>
2024-09-21 18:18:19 +0300

    last ready version hw-11

--- src/main.js
+++ src/main.js
@@ -1,19 +1,78 @@
 // Імпорт бібліотек
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import {  onButtonSubmit } from "./js/pixabay-api";
-// import { createGalleryMarkup } from "./js/render-functions";
+import { queryFunction } from "./js/pixabay-api";
+import { createGalleryMarkup } from "./js/render-functions";
+import "izitoast/dist/css/iziToast.min.css";
+import iziToast from "izitoast";
 
 const form = document.querySelector('form.js-search-form');
-export const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 
-export const showBox = new SimpleLightbox('.img-box a', { 
+const showBox = new SimpleLightbox('.img-box a', { 
     captions: true,
     captionsData: 'alt',
     captionDelay: 250,
     overlayOpacity: 0.7,
     className: 'lightbox',
    });
+
+   // Функції для запуску та закриття лоадера
+ function showLoader() {
+    document.getElementById('loader').style.display = 'block';
+    }
+    
+ function hideLoader() {
+        document.getElementById('loader').style.display = 'none';
+    }
+
+    // Колбек при натисканні кнопки + запит на сервер
+ function onButtonSubmit (event) {
+        event.preventDefault();
+        showLoader();
+    
+    const forma = event.currentTarget;
+    const {
+        searchValue: { value: query },
+      } = forma.elements;
+    console.log(query);
+
+     
+    // Перевірка на пустий інпут    
+    if (query === "") {
+        iziToast.show({
+            message: `⚠️ Please fill search input`,
+            position: 'topRight',
+            color: 'yellow',
+        })
+         hideLoader();
+    return
+    }
+     
+    //   зачистка попередньої видачі
+    gallery.innerHTML = "";  
+    
+    
+    queryFunction(query)
+      .then((photos) => {
+        if(!photos.hits || photos.hits.length === 0) {
+            iziToast.show({
+                title: "❌",
+                message: `"Sorry, there are no images matching your search query. Please try again!"`,
+                position: 'topRight',
+                color: 'red',
+            })
+            return
+        }
+        console.log(photos);
+        gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(photos.hits));
+        showBox.refresh();
+    })
+          
+      .catch((error) => console.log(error))
+      .finally(() => {hideLoader()}); 
+    }
+
 
 
 form.addEventListener('submit', onButtonSubmit);
